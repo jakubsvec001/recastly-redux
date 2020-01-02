2a00df200b623a1ecd67552da6149809e63b2b14
jakubsvec001 <jakubsvec001@gmail.com>
2020-01-02 15:00:12 -0800

    IMPLEMENT containers, rootReducer, and store

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './../reducers/main.js';
-import exampleVideoData from '../data/exampleVideoData.js';
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./../reducers/main.js";
+import exampleVideoData from "../data/exampleVideoData.js";
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
+
+const store = createStore(
+  rootReducer,
+  { currentVideo: null, videoList: [] },
+  applyMiddleware(thunk)
+);
+
+export default store;
