8bf7c49793acb1314cea949d9773e7ff770de404
jakubsvec001 <jakubsvec001@gmail.com>
2020-01-02 12:36:49 -0800

    Define inititial reducer implementation for currentVideo, videoList

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -1,7 +1,14 @@
 import Redux from 'redux';
 
-var currentVideoReducer = (state, action) => {
+var currentVideoReducer = (state = null, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  if(action.type === 'CHANGE_VIDEO') {
+    state = {
+      ...state,
+      currentVideo: action.video
+    };
+  }
+  return state;
 };
 
 export default currentVideoReducer;
